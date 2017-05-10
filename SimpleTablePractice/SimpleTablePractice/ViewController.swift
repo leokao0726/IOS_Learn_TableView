796edcb5735fdad1e28ab20d354dc68edff759bb
LeoKao <leokao0726@gmail.com>
2017-05-10 13:25:28 +0900

    init

---
+++ SimpleTablePractice/SimpleTablePractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTablePractice
+//
+//  Created by LeoKao on 2017/5/10.
+//  Copyright © 2017年 LeoKao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
