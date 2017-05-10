bf6e7ee4ac2268b60e6964bcbf4c70a19808759f
LeoKao <leokao0726@gmail.com>
2017-05-10 14:05:17 +0900

    simply implement the tableview

--- SimpleTablePractice/SimpleTablePractice/ViewController.swift
+++ SimpleTablePractice/SimpleTablePractice/ViewController.swift
@@ -8,7 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var restaurantNames = ["Cafe Deadend","Homei","Teakha","Cafe Loisl","Petite Oyster","For Kee Restaurant","Po's Atelier","Bourke Street Bakery","Upstate","Triaf"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return how many row you wanna show
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: "restaurant")
+        return cell
+        
+        //from indexPath.row we can get the row we want and set text
+    }
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
